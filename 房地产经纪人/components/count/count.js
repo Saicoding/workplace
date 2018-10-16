6dc234f21402f78be45069f6796408ce3e9144e0
Saicoding <417353147@qq.com>
2018-10-16 10:01:50 +0800

    增加count组件，增加答题收藏按钮(初步)

---
+++ 房地产经纪人/components/count/count.js
@@ -0,0 +1,42 @@
+// components/count/count.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    rightNum:{
+      type:Number,
+      value:0
+    },
+    wrongNum:{
+      type:Number,
+      value:0
+    },
+    px:{
+      type:Number,
+      value:0
+    },
+    nums:{
+      type:Number,
+      value:0
+    },
+    isMark:{
+      type:Boolean,
+      value:false
+    } 
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
