143319d111ee0f7fd3b09aae60f4fa649993c0e2
Saicoding <417353147@qq.com>
2018-12-05 17:29:57 +0800

    增加雷达页面的时间选项

--- 房地产经纪人/pages/mine/mineRadar/mineRadar.js
+++ 房地产经纪人/pages/mine/mineRadar/mineRadar.js
@@ -1,4 +1,5 @@
 // pages/hasNoErrorShiti/hasNoErrorShiti.js
+var dateTimePicker = require('../../../utils/dateTimePicker.js');
 
 const API_URL = 'https://xcx2.chinaplat.com/'; //接口地址
 const app = getApp();
@@ -30,14 +31,28 @@ Page({
       ["考前秘籍", 0],
       ["考点学习", 0]
     ],
-    loaded:false
-
+    loaded:false,
+    date: '2018-10-01',
+    time: '12:00',
+    dateTimeArray: null,
+    dateTime: null,
+    dateTimeArray1: null,
+    dateTime1: null,
+    startYear: 2018,
+    endYear: new Date().getFullYear()
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
+    // 获取完整的年月日 时分秒，以及默认显示的数组
+    var obj = dateTimePicker.dateTimePicker(this.data.startYear, this.data.endYear);
+    var obj1 = dateTimePicker.dateTimePicker(this.data.startYear, this.data.endYear);
+    // 精确到分的处理，将数组的秒去掉
+    var lastArray = obj1.dateTimeArray.pop();
+    var lastTime = obj1.dateTime.pop();
+
     wx.setNavigationBarTitle({
       title: options.title,
     })
@@ -50,7 +65,11 @@ Page({
     self.getStudyRate(user,kmid);
 
     self.setData({
-      user: user
+      user: user,
+      dateTime: obj.dateTime,
+      dateTimeArray: obj.dateTimeArray,
+      dateTimeArray1: obj1.dateTimeArray,
+      dateTime1: obj1.dateTime
     })
 
   },
@@ -75,7 +94,6 @@ Page({
     let self = this;
 
     app.post(API_URL, "action=MyLearningProSin&username=" + user.username + "&acode=" + user.acode+"&kmid="+kmid, false, false, "").then((res) => {
-      
       let chanelArray = self.data.chanelArray;
       let rate = res.data.data[0];
       chanelArray[0][1] = rate.zhangjie;
@@ -227,5 +245,21 @@ Page({
       radCtx.fill();
     }
 
+  },
+
+  changeDateTime1(e) {
+    this.setData({ dateTime1: e.detail.value });
+  },
+
+  changeDateTimeColumn1(e) {
+    var arr = this.data.dateTime1, dateArr = this.data.dateTimeArray1;
+
+    arr[e.detail.column] = e.detail.value;
+    dateArr[2] = dateTimePicker.getMonthDay(dateArr[0][arr[0]], dateArr[1][arr[1]]);
+
+    this.setData({
+      dateTimeArray1: dateArr,
+      dateTime1: arr
+    });
   }
 })
